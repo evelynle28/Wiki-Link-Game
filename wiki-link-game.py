0beae9a486f15ede45a127a02a86f9303d1b5182
Evelyn Le <hathu.ams@gmail.com>
2019-11-06 18:19:34 -0600

    documented and shortened wiki link game

--- wiki-link-game.py
+++ wiki-link-game.py
@@ -1,44 +1,45 @@
 import wikipedia as wiki
 import random
-link_set = set()
 
-flag = False
-while (flag == False):
+link_set = set() #Set containing the links that have been through
+valid = False
+
+#Prompt user to enter the topic until it is valid
+while (valid == False):
   page_name = input("Enter the starting page: ")
   try:
     page = wiki.WikipediaPage(page_name)
   except wiki.exceptions.DisambiguationError as e:
+    print ("Randomly pick one from the options.")
     page = wiki.WikipediaPage(random.choice(e.options))
-    flag = True
+    valid = True
   except wiki.exceptions.PageError:
     print ("Page does not exist!")
   else:
-    flag = True
+    valid = True
 
-links_on_page = page.links
-count = 0
-
-valid = True
-while (valid):
-  if (len(links_on_page) <10):
+count = 0 #Counter for the number of links that the program has run into
+final = False
+while (not final):
+  #Check if there is enough 10 links in the current page
+  if (len(page.links) <10):
     print ("There are not enough 10 links in this page")
-    valid = False
+    final = True
   else:
+    #Take the 10th link in the list of links
     link_title = page.links[9]
     print (link_title)
     if (link_title in link_set):
       print ("We have already encountered this")
-      valid = False
+      final = True
     else:
-      count += 1
-      link_set.add(link_title)
+      count += 1 #update the number of valid link
+      link_set.add(link_title) #Add the new link to the link_set
       try:
         page = wiki.WikipediaPage(link_title)
       except wiki.exceptions.PageError:
         print("This page has no content");
-        valid = False
-      else:
-        links_on_page = page.links
+        final = True
 
 print ("Total number of points:", count)
 
